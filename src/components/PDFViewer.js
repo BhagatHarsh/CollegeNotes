43ce96e12b259761909ece96465f3f6be79fe676
Harsh Bhagat <93080554+BhagatHarsh@users.noreply.github.com>
2023-08-31 09:10:48 +0530

    update: added loading animation and few more slides

--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Worker, Viewer } from '@react-pdf-viewer/core';
 import { pdfjs } from 'react-pdf';
+import { PacmanLoader } from 'react-spinners';
 import './PDFViewer.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
@@ -8,6 +9,7 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/b
 const PDFViewer = ({ src }) => {
     const [numPages, setNumPages] = useState(null);
     const [renderedPages, setRenderedPages] = useState(1);
+    const [isLoading, setIsLoading] = useState(true);
 
     const handleLoadSuccess = ({ numPages }) => {
         setNumPages(numPages);
@@ -15,19 +17,33 @@ const PDFViewer = ({ src }) => {
 
     useEffect(() => {
         const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 2000); // Show the loader for 5 seconds
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
             if (renderedPages < numPages) {
                 setRenderedPages(renderedPages + 1);
             }
-        }, 2000); // Delay rendering of each page for 2 seconds
+        }, []); // Delay rendering of each page for 2 seconds
 
         return () => clearTimeout(timer);
     }, [renderedPages, numPages]);
 
     return (
         <div className="pdf-viewer-container">
-            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.9.179/build/pdf.worker.min.js">
-                <Viewer fileUrl={src + '?wrap=0'} />
-            </Worker>
+            {isLoading ? (
+                <div className="loading-bar">
+                    <PacmanLoader color="#00BFFF" size={40} />
+                </div>
+            ) : (
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.9.179/build/pdf.worker.min.js">
+                    <Viewer fileUrl={src + '?wrap=0'} onDocumentLoadSuccess={handleLoadSuccess} />
+                </Worker>
+            )}
         </div>
     );
 };
